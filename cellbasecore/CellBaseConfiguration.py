f33ec3908aeb404d2a7cab0465e9fa35a75f4d09
Javi Lopez <javild@gmail.com>
2015-10-20 15:21:38 +0100

    develop: started first approach to python client

--- cellbasecore/CellBaseConfiguration.py
+++ cellbasecore/CellBaseConfiguration.py
@@ -1,1 +1,10 @@
 __author__ = 'fjlopez'
+
+import json
+
+class CellBaseConfiguration(dict):
+    def __init__(self, configFileName):
+        fdw = open(configFileName)
+        dictionaryString = fdw.readlines(fdw)
+        fdw.close()
+        dict.__init__(self, json.loads(dictionaryString))
